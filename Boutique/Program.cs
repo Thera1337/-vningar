065c675dd870017e0dc5e743c08cefbe0c7db66f
Thera1337 <70898199+Thera1337@users.noreply.github.com>
2020-10-12 11:34:49 +0200

    Update Program.cs

--- Boutique/Program.cs
+++ Boutique/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 
 namespace Boutique
 {
     class Program
     {
+        static int Choise;
+        static int TotalPrice;
         static List<Plagg> Butik = new List<Plagg>();
         static List<Plagg> ShoppingCart = new List<Plagg>();
         static void Main(string[] args)
         {
             Admin();
+            Console.Clear();
+            while (true)
+            {
+                PrintBoutique();
+                Boutique();
+                Thread.Sleep(2000);
+                Console.SetCursorPosition(0, 0);
+            }
         }
         static void Admin()
         {
@@ -26,8 +37,8 @@ namespace Boutique
                 Console.WriteLine("[3] Socks");
                 Console.WriteLine("[4] Scarf");
                 int type = int.Parse(Console.ReadLine());
-                plagg.Type = Enum.GetName(typeof(Type), type);
-                //plagg.Type2 = (Type)2;
+                //plagg.Kind = Enum.GetName(typeof(Kind), type);
+                plagg.Kind = (Type)type;
 
                 Console.WriteLine("Välj storlek på plagget: ");
                 Console.WriteLine("[1] XS");
@@ -36,7 +47,8 @@ namespace Boutique
                 Console.WriteLine("[4] L");
                 Console.WriteLine("[5] XL");
                 int size = int.Parse(Console.ReadLine());
-                plagg.Size = Enum.GetName(typeof(Size), size);
+                //plagg.Size = Enum.GetName(typeof(Size), size);
+                plagg.Size = (Sizes)size;
 
                 Console.WriteLine("Välj färg på plagget: ");
                 Console.WriteLine("[1] Blue");
@@ -44,33 +56,83 @@ namespace Boutique
                 Console.WriteLine("[3] Black");
                 Console.WriteLine("[4] White");
                 int color = int.Parse(Console.ReadLine());
-                plagg.Color = Enum.GetName(typeof(Color), color);
+                //plagg.Color = Enum.GetName(typeof(Color), color);
+                plagg.Color = (Colors)color;
 
                 Console.WriteLine("Välj pris på plagget: ");
-                Console.WriteLine("[1] One Hundred");
-                Console.WriteLine("[2] Two Hundred");
-                Console.WriteLine("[3] Three Hundred");
-                Console.WriteLine("[4] Four Hundred");
+                Console.WriteLine("[100] One Hundred");
+                Console.WriteLine("[200] Two Hundred");
+                Console.WriteLine("[300] Three Hundred");
+                Console.WriteLine("[400] Four Hundred");
                 int price = int.Parse(Console.ReadLine());
-                plagg.Price = Enum.GetName(typeof(Price), price);
+                //plagg.Price = Enum.GetName(typeof(Price), price);
+                plagg.Price = (Prices)price;
 
                 Butik.Add(plagg);
             }
         }
+        static void PrintBoutique()
+        {
+            int i = 0;
+            foreach (Plagg plagg in Butik)
+            {
+                if (i == Choise)
+                {
+                    Console.WriteLine($"---> Plagg: {plagg.Kind}, Färg: {plagg.Color}, Storlek: {plagg.Size}, Pris: {plagg.Price}");
+                }
+                else
+                {
+                    Console.WriteLine($"Plagg: {plagg.Kind}, Färg: {plagg.Color}, Storlek: {plagg.Size}, Pris: {plagg.Price}");
+
+                }
+                i++;
+            }
+            Console.WriteLine("Varukorg:");
+            foreach (Plagg plagg1 in ShoppingCart)
+            {
+                Console.WriteLine($"Plagg: {plagg1.Kind}, Pris: {plagg1.Price}");
+                TotalPrice += (int)plagg1.Price;
+            }
+        }
+        static void Boutique()
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            switch (key.KeyChar)
+            {
+                case 'a':
+                    Choise--;
+                    if (Choise < 0)
+                    {
+                        Choise = Butik.Count - 1;
+                    }
+                    break;
+                case 'z':
+                    Choise++;
+                    if (Choise == Butik.Count)
+                    {
+                        Choise = 0;
+                    }
+                    break;
+                case '\r':
+                    ShoppingCart.Add(Butik[Choise]);
+                    break;
+                default:
+                    break;
+            }
+        }
         struct Plagg
         {
-            public string Type { get; set; }
-            //public Type Type2 { get; set; }
-            public string Size { get; set; }
-            public string Color { get; set; }
-            public string Price { get; set; }
+            public Type Kind { get; set; }
+            public Sizes Size { get; set; }
+            public Colors Color { get; set; }
+            public Prices Price { get; set; }
 
-            public Plagg(string type, string size, string color, string price)
+            public Plagg(int kind, int size, int color, int price)
             {
-                Type = type;
-                Size = size;
-                Color = color;
-                Price = price;
+                Kind = (Type)kind;
+                Size = (Sizes)size;
+                Color = (Colors)color;
+                Price = (Prices)price;
             }
         }
         enum Type
@@ -80,7 +142,7 @@ namespace Boutique
             Socks,
             Scarf
         }
-        enum Size
+        enum Sizes
         {
             XS = 1,
             S,
@@ -88,19 +150,19 @@ namespace Boutique
             L,
             XL,
         }
-        enum Color
+        enum Colors
         {
             Blue = 1,
             Green,
             Black,
             White
         }
-        enum Price
+        enum Prices
         {
-            OneHundred = 1,
-            TwoHundred,
-            ThreeHundred,
-            FourHundred
+            OneHundred = 100,
+            TwoHundred = 200,
+            ThreeHundred = 300,
+            FourHundred= 400
         }
     }
 }
